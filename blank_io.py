955ff58dd5b1d17b0531b5785f8861111f197705
PaulineDataWard <polo@paulineward.me.uk>
2023-05-30 14:57:52 +0100

    sketched out python file
    Basic algorithm in python, read in, merge, write output.

---
+++ blank_io.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 30 14:42:16 2023
+
+@author: PaulineDataWard
+
+Script to read in two files, merge and then output a resulting dataset. 
+"""
+
+import pandas 
+import os
+
+
+def func_read_in(): 
+        # Read in pattypan output file to a dataframe
+        
+        # Read in source repo metadata to a dataframe
+     
+def func_combine():
+        # Transform the filename to allow it to be used as a common column
+        
+        
+        # Merge the dataframes to 'join' on the filename
+        
+def func_write():        
+        # Output a table to be added into the pattypan spreadsheet 
+
+
+if __name__=="__main__": 
+    func_read_in()
+    func_combine()
+    func_write() 
+    
