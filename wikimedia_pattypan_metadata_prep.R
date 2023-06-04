915f01b8c1f5d3a707b8886fc4548c235dfd9ed2
PaulineDataWard <polo@paulineward.me.uk>
2023-06-04 13:35:06 +0100

    Filtered out images already imported
    The Aberdeen photos collection 3304 were uploaded to wikimedia a couple of years ago. So I excluded those rows by filtering out where 3304 was present in the collection column.

--- wikimedia_pattypan_metadata_prep.R
+++ wikimedia_pattypan_metadata_prep.R
@@ -6,6 +6,7 @@ library(here)
 library(readr) 
 library(janitor)
 library(readxl)
+library(stringr)
 
 # Read in metadata from source repo CSV
 # df_datashare_metadata <- read_csv(here("metadata", "source_repo_raw_export_UK_comp_collecn10283-4857.csv"))
@@ -31,16 +32,21 @@ tbl_source_repo_raw_export_UK_comp_collecn10283_4857 <- read_csv("metadata/sourc
                                                                               `ds.not-emailable.item[en]` = col_skip())) %>% clean_names()
 
 
-print("Imported source metadata is a tibble: ", is_tibble(tbl_source_repo_raw_export_UK_comp_collecn10283_4857))
+print("Imported source metadata is a tibble: ")
+print(as.character(is_tibble(tbl_source_repo_raw_export_UK_comp_collecn10283_4857)))
 
 # Read in metadata from pattypan with readexcel()
-v1_2_PW_pattypan_2023_05_29_08_10_23 <- read_excel("metadata/v1-2_PW_pattypan 2023-05-29 08_10_23.xls", 
+tbl_v1_2_PW_pattypan_2023_05_29_08_10_23 <- read_excel("metadata/v1-2_PW_pattypan 2023-05-29 08_10_23.xls", 
                                                    col_types = c("text", "text", "text", 
                                                                  "text", "text", "skip", "text", "skip", 
                                                                  "skip"))
-View(v1_2_PW_pattypan_2023_05_29_08_10_23)
-print("Imported pattypan table is a tibble: ", is_tibble(v1_2_PW_pattypan_2023_05_29_08_10_23))
+print("Imported pattypan table is typeof: ")
+print(typeof(tbl_v1_2_PW_pattypan_2023_05_29_08_10_23))
   
+# Identify and then exclude the images already on wikimedia ie Aberdeen collection
+filtered_tbl_source_repo_UK_comp_collection <- tbl_source_repo_raw_export_UK_comp_collecn10283_4857 %>%
+  filter(! str_detect(collection, "3304"))
+
 # Combine by join on the image filename
 
 # OUtput to a new file, for pasting into the xls
