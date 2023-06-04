219696885c50b88876d529396bde3493afbe4764
PaulineDataWard <polo@paulineward.me.uk>
2023-06-04 11:18:43 +0100

    Fixed import bugs
    Configured import to skip correct cols, and treat all string fields as character cols not logical.

--- wikimedia_pattypan_metadata_prep.R
+++ wikimedia_pattypan_metadata_prep.R
@@ -5,11 +5,12 @@ library(tidyverse)
 library(here) 
 library(readr) 
 library(janitor)
+library(readxl)
 
 # Read in metadata from source repo CSV
 # df_datashare_metadata <- read_csv(here("metadata", "source_repo_raw_export_UK_comp_collecn10283-4857.csv"))
 
-tbl_source_repo_raw_export_UK_comp_collecn10283_4857 <-    read_csv("metadata/source_repo_raw_export_UK_comp_collecn10283-4857.csv", 
+tbl_source_repo_raw_export_UK_comp_collecn10283_4857 <- read_csv("metadata/source_repo_raw_export_UK_comp_collecn10283-4857.csv", 
                                                              col_types = cols(`dc.contributor.other[en_UK]` = col_character(), 
                                                                               `dc.contributor[en]` = col_character(), 
                                                                               `dc.coverage.spatial[en]` = col_character(), 
@@ -26,12 +27,20 @@ tbl_source_repo_raw_export_UK_comp_collecn10283_4857 <-    read_csv("metadata/so
                                                                               `dc.subject.classification[en_UK]` = col_character(), 
                                                                               `dc.subject[en_UK]` = col_character(), 
                                                                               `dc.title[en_UK]` = col_character(), 
-                                                                              `dc.type[en_UK]` = col_character(), 
+                                                                              `dc.type[]` = col_skip(), `dc.type[en_UK]` = col_skip(), 
                                                                               `ds.not-emailable.item[en]` = col_skip())) %>% clean_names()
-                                                              
+
+
+print("Imported source metadata is a tibble: ", is_tibble(tbl_source_repo_raw_export_UK_comp_collecn10283_4857))
 
 # Read in metadata from pattypan with readexcel()
-
+v1_2_PW_pattypan_2023_05_29_08_10_23 <- read_excel("metadata/v1-2_PW_pattypan 2023-05-29 08_10_23.xls", 
+                                                   col_types = c("text", "text", "text", 
+                                                                 "text", "text", "skip", "text", "skip", 
+                                                                 "skip"))
+View(v1_2_PW_pattypan_2023_05_29_08_10_23)
+print("Imported pattypan table is a tibble: ", is_tibble(v1_2_PW_pattypan_2023_05_29_08_10_23))
+  
 # Combine by join on the image filename
 
 # OUtput to a new file, for pasting into the xls
